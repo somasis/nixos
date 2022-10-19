f0c1d2b201d0246a32f5876f56de4dc3315a363d
Kylie McClain <kylie@somas.is>
2022-10-19 01:15:22 -0400

    somasis/tmux: remove unnecessary todo about tmux

--- users/somasis/tmux.nix
+++ users/somasis/tmux.nix
@@ -2,7 +2,6 @@
 , pkgs
 , ...
 }: {
-  # TODO: manage tmux via home-manager
   home.packages = [ pkgs.tmux ];
   home.persistence."/persist${config.home.homeDirectory}".directories = [ "etc/tmux" ];
 }
