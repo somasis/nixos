3327af292dac7074a290426312e16dd789d15518
Kylie McClain <kylie@somas.is>
2022-10-19 01:14:28 -0400

    music/manage: add gazelle-origin

--- users/somasis/music/manage.nix
+++ users/somasis/music/manage.nix
@@ -46,22 +46,45 @@ let
           >> "$runtime"/beets.yml
     '';
   });
+
+  gazelle-origin = (pkgs.python3Packages.buildPythonApplication rec {
+    pname = "gazelle-origin";
+    version = "unstable-2020-11-16";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "x1ppy";
+      repo = pname;
+      rev = "4bfffa575ace819b02d576e9f0b79d20335c03c5";
+      hash = "sha256-0000000000000000000000000000000000000000000=";
+    };
+
+    propagatedBuildInputs = [
+      pkgs.python3Packages.setuptools
+    ];
+
+    meta = with pkgs.lib;      {
+      description = "Gazelle origin.yaml generator";
+      homepage = "https://github.com/x1ppy/gazelle-origin";
+      license = licenses.unfree;
+      maintainers = with maintainers; [ somasis ];
+    };
+  });
 in
 {
   home.packages = [
     pass-beets
+    gazelle-origin
   ];
 
   programs.beets = {
     enable = true;
-    # package =
-    #   (pkgs.beets.override {
-    #     pluginOverrides = {
-    #       # originquery = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.originquery ]; };
-    #       # check = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.check ]; };
-    #       barcode = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.barcode ]; };
-    #     };
-    #   });
+    package = (pkgs.beets.override {
+      pluginOverrides = {
+        # check = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.check ]; };
+        barcode = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.barcode ]; };
+        originquery = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.originquery ]; };
+      };
+    });
 
     settings = rec {
       library = "${config.programs.beets.settings.directory}/beets.db";
@@ -72,10 +95,10 @@ in
       sort_case_insensitive = false;
 
       plugins = [
-        # "barcode"
         # "check"
-        # "originquery"
         # "web"
+        "barcode"
+        "originquery"
 
         # acousticbrainz/acoustid: apikey comes from `pass-beets`
         "acousticbrainz"
@@ -105,80 +128,94 @@ in
       };
 
       convert = {
+        # auto = true;
 
+        copy_album_art = true;
+        embed = false;
+        album_art_maxwidth = "1024";
+        quiet = true;
 
-        # originquery = {
-        #   origin_file = "origin.yaml";
-        #   tag_patterns = {
-        #     media = ''$.Media'';
-        #     year = ''$."Edition year"'';
-        #     label = ''$."Record label"'';
-        #     catalognum = ''$."Catalog number"'';
-        #     albumdisambig = ''$.Edition'';
-        #   };
-        # };
-
-        import = {
-          # Search
-          languages = [ "tok" "en" "jp" ];
-
-          # Interactive mode
-          bell = true;
-          detail = true;
-          timid = true;
-
-          # File manipulation
-          write = true;
-          copy = true;
-          move = false;
-
-          incremental = true;
-          resume = false;
+        format = "opus";
+        formats = {
+          opus.command = "ffmpeg -i $source -y -vn -acodec libopus -ab 96k -ar 48000 $dest";
+          opus.extension = "opus";
         };
-
-        match = {
-          # distance_weights.barcode = 1.0;
-
-          max_rec = {
-            missing_tracks = "medium";
-            unmatched_tracks = "medium";
-            tracks = "medium";
-          };
-        };
-
-        paths = {
-          # _$albumtype/
-          default = "$albumartist - $album%if{$original_year, ($original_year)}/$track - $artist - $title";
-          "singleton:true" = "_single/$artist/$title";
-          "comp:true" = "_compilation/$albumartist - $title/$track - $artist - $title";
-          "albumtype:soundtrack" = "_soundtracks/$albumartist - $album%if{$original_year, ($original_year)}/$track - $artist - $title";
-        };
-      }
-      // lib.optionalAttrs config.services.mopidy.enable { mpd.host = config.services.mopidy.settings.mpd.hostname; }
-      ;
-    };
-
-    systemd.user.services."pass-beets" = {
-      Unit = {
-        Description = "Authenticate `beets` using `pass`";
-        PartOf = [ "default.target" ];
       };
-      Install.WantedBy = [ "default.target" ];
 
-      Service = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-
-        ExecStart = [ "${pass-beets}/bin/pass-beets" ];
-        ExecStop = [ "${pkgs.coreutils}/bin/rm -rf %t/pass-beets" ];
+      originquery = {
+        origin_file = "origin.yaml";
+        tag_patterns = {
+          media = ''$.Media'';
+          year = ''$."Edition year"'';
+          label = ''$."Record label"'';
+          catalognum = ''$."Catalog number"'';
+          albumdisambig = ''$.Edition'';
+        };
       };
-    };
 
-    # systemd.user.services.beets-web = {
-    #   Unit.Description = "beets' web interface";
-    #   Install.WantedBy = [ "default.target" ];
-    #   Unit.PartOf = [ "default.target" ];
-    #   Service.Type = "simple";
-    #   Service.ExecStart = "${config.programs.beets.package}/bin/beet web";
-    # };
-  }
+      import = {
+        # Search
+        languages = [ "tok" "en" "jp" ];
+
+        # Interactive mode
+        bell = true;
+        detail = true;
+        timid = true;
+
+        # File manipulation
+        write = true;
+        copy = true;
+        move = false;
+
+        incremental = true;
+        resume = false;
+      };
+
+      match = {
+        # distance_weights.barcode = 1.0;
+
+        max_rec = {
+          missing_tracks = "medium";
+          unmatched_tracks = "medium";
+          tracks = "medium";
+        };
+      };
+
+      paths = {
+        # _$albumtype/
+        default = "$albumartist - $album%if{$original_year, ($original_year)}/$track - $artist - $title";
+        "singleton:true" = "_single/$artist/$title";
+        "comp:true" = "_compilation/$albumartist - $title/$track - $artist - $title";
+        "albumtype:soundtrack" = "_soundtracks/$albumartist - $album%if{$original_year, ($original_year)}/$track - $artist - $title";
+      };
+    }
+    // lib.optionalAttrs config.services.mopidy.enable { mpd.host = config.services.mopidy.settings.mpd.hostname; }
+    ;
+  };
+
+  systemd.user.services.beets-web = {
+    Unit = {
+      Description = "beets' web interface";
+      PartOf = [ "default.target" ];
+    };
+    Install.WantedBy = [ "default.target" ];
+    Service.Type = "simple";
+    Service.ExecStart = "${config.programs.beets.package}/bin/beet web";
+  };
+
+  systemd.user.services."pass-beets" = {
+    Unit = {
+      Description = "Authenticate `beets` using `pass`";
+      PartOf = [ "default.target" ];
+    };
+    Install.WantedBy = [ "default.target" ];
+
+    Service = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+
+      ExecStart = [ "${pass-beets}/bin/pass-beets" ];
+      ExecStop = [ "${pkgs.coreutils}/bin/rm -rf %t/pass-beets" ];
+    };
+  };
+}
