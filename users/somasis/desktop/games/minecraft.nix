df15a472affe52c490863236ff2bac3d2dced715
Kylie McClain <kylie@somas.is>
2022-10-23 21:18:33 -0400

    minecraft: switch to Prism Launcher

--- users/somasis/desktop/games/minecraft.nix
+++ users/somasis/desktop/games/minecraft.nix
@@ -1,13 +1,12 @@
 { pkgs, config, ... }: {
   home.packages = [
-    # pkgs.polymc
+    pkgs.prismlauncher
     pkgs.jdk
   ];
 
-  home.persistence."/persist${config.home.homeDirectory}".directories = [
-    "share/PolyMC"
-  ];
+  home.persistence."/persist${config.home.homeDirectory}".directories = [ "share/PolyMC" ];
 
+  # TODO use NixMinecraft?
   # programs.minecraft = {
   #   shared = {
   #     username = "somasis";
