ca87a6f4523e4d8e65183ecec7d986b613f6c556
Kylie McClain <kylie@somas.is>
2022-10-25 00:47:43 -0400

    ilo.somas.is/fwupd: enable updates for Framework BIOS

--- hosts/ilo.somas.is/hardware/fwupd.nix
+++ hosts/ilo.somas.is/hardware/fwupd.nix
@@ -1,11 +1,11 @@
 {
   # Keep system firmware up to date.
   # TODO: Framework still doesn't have their updates in LVFS properly,
-  #       and, I can't enable the LVFS testing channel because it's
-  #       part of the nix-managed configuration.
   #       <https://knowledgebase.frame.work/en_us/framework-laptop-bios-releases-S1dMQt6F#:~:text=Updating%20via%20LVFS%20is%20available%20in%20the%20testing%20channel>
-  #       <https://github.com/NixOS/nixpkgs/issues/158497>
-  services.fwupd.enable = true;
+  services.fwupd = {
+    enable = true;
+    extraRemotes = [ "lvfs-testing" ];
+  };
 
   environment.persistence."/cache".directories = [ "/var/lib/fwupd" ];
 }
