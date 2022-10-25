1b3adc7773339f1e8796889741a13790df82024e
Kylie McClain <kylie@somas.is>
2022-10-24 20:51:30 -0400

    kakoune: add shell lint/format tools to user env

--- users/somasis/kakoune/filetype/shell.nix
+++ users/somasis/kakoune/filetype/shell.nix
@@ -1,65 +1,103 @@
 { pkgs, ... }:
 let
-  shellcheckfmt = pkgs.writeShellScript "shellcheckfmt" ''
-    d=$(mktemp -d)
+  shellcheckfmt = pkgs.writeShellApplication {
+    name = "shellcheckfmt";
+    runtimeInputs = [
+      pkgs.shellcheck
+      pkgs.coreutils
+      pkgs.diffutils
+    ];
 
-    for f; do
-        if [ "$f" = '-' ]; then
-            cat "$f" >"$d"/input
-            f="$d"/input
-        fi
+    text = ''
+      d=$(mktemp -d)
 
-        # for when shellcheck finds issues, but can't auto-fix any of them.
-        ${pkgs.shellcheck}/bin/shellcheck -f diff -x -a "$f" >/dev/null 2>&1 || e=$?
+      for f; do
+          if [ "$f" = '-' ]; then
+              cat "$f" >"$d"/input
+              f="$d"/input
+          fi
 
-        # (there's a colon in the next line because otherwise it thinks this is a directive!
-        #:shellcheck(1) says...
-        # > RETURN VALUES
-        # >        ShellCheck uses the following exit codes:
-        # >
-        # >        - 0: All files successfully scanned with no issues.
-        # >
-        # >        - 1: All files successfully scanned with some issues.
-        # >
-        # >        - 2: Some files could not be processed (e.g.  file not found).
-        if [ ''${e:-0} -gt 1 ]; then
-            cat "$f"
-            continue
-        fi
+          # for when shellcheck finds issues, but can't auto-fix any of them.
+          shellcheck -f diff -x -a "$f" >/dev/null 2>&1 || e=$?
 
-        mkfifo "$d"/diff
-        ${pkgs.shellcheck}/bin/shellcheck -f diff -x -a "$f" >"$d"/diff &
-        patch -p1 -s -i "$d"/diff -o - "$f" &
-        wait
-        rm -f "$d"/diff "$d"/input
-    done
-    rm -rf "$d"
-  '';
-  format = pkgs.writeShellScript "format" ''
-    trap 'rm -f "$err" "$orig" "$new"' EXIT
+          # (there's a colon in the next line because otherwise it thinks this is a directive!
+          #:shellcheck(1) says...
+          # > RETURN VALUES
+          # >        ShellCheck uses the following exit codes:
+          # >
+          # >        - 0: All files successfully scanned with no issues.
+          # >
+          # >        - 1: All files successfully scanned with some issues.
+          # >
+          # >        - 2: Some files could not be processed (e.g.  file not found).
+          if [ ''${e:-0} -gt 1 ]; then
+              cat "$f"
+              continue
+          fi
 
-    err=$(mktemp)
-    orig=$(mktemp)
-    new=$(mktemp)
-    cat > "$orig"
+          mkfifo "$d"/diff
+          shellcheck -f diff -x -a "$f" >"$d"/diff &
+          patch -p1 -s -i "$d"/diff -o - "$f" &
+          wait
+          rm -f "$d"/diff "$d"/input
+      done
+      rm -rf "$d"
+    '';
+  };
 
-    { ${shellcheckfmt} "$orig" 2>>"$err" || cat "$orig"; } | ${pkgs.shfmt}/bin/shfmt - >"$new" 2>>"$err"
+  format = pkgs.writeShellApplication {
+    name = "shformat";
 
-    if [ "$(wc -c < "$new")" -eq 0 ]; then
-        cat "$orig"
-        cat "$err" >&2
-        exit 1
-    else
-        cat "$new"
-    fi
-  '';
-  lint = pkgs.writeShellScript "lint" ''
-    (
-        ${pkgs.checkbashisms}/bin/checkbashisms -l "$@" &
-        ${pkgs.shellcheck}/bin/shellcheck -f gcc -x "$@" &
-        wait
-    ) | sort
-  '';
+    runtimeInputs = [
+      pkgs.coreutils
+      pkgs.shfmt
+      shellcheckfmt
+    ];
+
+    text = ''
+      trap 'rm -f "$err" "$orig" "$new"' EXIT
+
+      err=$(mktemp)
+      orig=$(mktemp)
+      new=$(mktemp)
+      cat > "$orig"
+
+      { shellcheckfmt "$orig" 2>>"$err" || cat "$orig"; } | shfmt - >"$new" 2>>"$err"
+
+      if [ "$(wc -c < "$new")" -eq 0 ]; then
+          cat "$orig"
+          cat "$err" >&2
+          exit 1
+      else
+          cat "$new"
+      fi
+    '';
+  };
+
+  lint = pkgs.writeShellApplication {
+    name = "shlint";
+
+    runtimeInputs = [
+      pkgs.checkbashisms
+      pkgs.coreutils
+      pkgs.shellcheck
+    ];
+
+    text = ''
+      (
+          (
+              set +e
+              checkbashisms -l "$@" 2>/dev/null
+              e=$?
+              [[ "$e" -ne 4 ]] || exit "$e" # exit 4 == "No bashisms were detected in a bash script."
+              exit 0
+          ) &
+
+          shellcheck -f gcc -x "$@" &
+          wait
+      ) | sort
+    '';
+  };
 in
 {
   programs.kakoune.config.hooks = [
@@ -67,8 +105,8 @@ in
       name = "WinSetOption";
       option = "filetype=sh";
       commands = ''
-        set-option window formatcmd "${format}"
-        set-option window lintcmd "${lint}"
+        set-option window formatcmd "${format}/bin/shformat"
+        set-option window lintcmd "${lint}/bin/shlint"
       '';
     }
   ];
@@ -77,4 +115,13 @@ in
     enable=all
     disable=SC2249
   '';
+
+  home.packages = [
+    pkgs.checkbashisms
+    pkgs.shellcheck
+    pkgs.shfmt
+    shellcheckfmt
+    format
+    lint
+  ];
 }
