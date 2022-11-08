717a308741c2dad7c8bdd0fd24505ed4707ddcbf
Kylie McClain <kylie@somas.is>
2022-11-07 22:47:20 -0500

    somasis/desktop/mess: use store path

--- users/somasis/desktop/mess.nix
+++ users/somasis/desktop/mess.nix
@@ -54,7 +54,7 @@ in
 
       Service = {
         Type = "oneshot";
-        ExecStart = "mess";
+        ExecStart = "${mess}/bin/mess";
         ExecStartPost = [
           ''${pkgs.coreutils}/bin/mkdir -p "${config.home.homeDirectory}/mess/current/incoming"''
           ''${pkgs.coreutils}/bin/mkdir -p "${config.home.homeDirectory}/mess/current/src"''
