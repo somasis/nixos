5a6eb1c53bac4c16511b90d6a8732d886449f46a
Kylie McClain <kylie@somas.is>
2022-11-11 16:53:08 -0500

    music/play: update
    this is a very nonspecific commit because I'm not even sure I'm gonna
    keep with mopidy, since mopidy-mpd seems quite bad.

--- users/somasis/music/play.nix
+++ users/somasis/music/play.nix
@@ -1,25 +1,55 @@
 { nixosConfig, config, pkgs, ... }:
 let
   xdgRuntimeDir = "/run/user/${toString nixosConfig.users.users.${config.home.username}.uid}";
+
+  pass-mopidy-subidy = (pkgs.writeShellApplication {
+    name = "pass-mopidy-subidy";
+    runtimeInputs = [
+      config.programs.password-store.package
+      pkgs.coreutils
+    ];
+
+    text = ''
+      set -eu
+      set -o pipefail
+
+      umask 0077
+
+      : "''${XDG_CONFIG_HOME:=$HOME/.config}"
+      : "''${XDG_RUNTIME_DIR:=/run/user/$(id -un)}"
+
+      pass "$1" >/dev/null 2>&1 || exit 1
+
+      cat > "$XDG_RUNTIME_DIR"/pass-mopidy-subidy.ini <<EOF
+      [subidy]
+      password = $(pass "$1")
+      EOF
+    '';
+  });
 in
 {
   services.mopidy = {
     enable = true;
     extensionPackages = [
-      pkgs.mopidy-mpd
-      pkgs.mopidy-scrobbler
-      pkgs.mopidy-somafm
       # pkgs.mopidy-beets
-      pkgs.mopidy-iris
+      # pkgs.mopidy-scrobbler
+      # pkgs.mopidy-somafm
+
+      # pkgs.mopidy-iris
+
+      # pkgs.mopidy-local
+      pkgs.mopidy-mpd
+      pkgs.mopidy-subidy
     ];
 
     settings = {
       core.restore_state = true;
 
-      iris.enabled = true;
-
       file = {
-        media_dirs = [ config.xdg.userDirs.music ];
+        enabled = true;
+        media_dirs = config.xdg.userDirs.music;
+        # album_art_files = [ "cover" ]
+
         excluded_file_extensions = [
           ".html"
           ".zip"
@@ -34,16 +64,53 @@ in
         ];
       };
 
+      http.enabled = true;
+
+      # iris = {
+      #   enabled = true;
+      #   country = "us";
+      #   locale = "en_US";
+      # };
+
       # https://github.com/mopidy/mopidy-mpd#configuration
       mpd = {
         enabled = true;
         hostname = "unix:${xdgRuntimeDir}/mopidy-mpd.sock";
+        command_blacklist = [ ];
+      };
+
+      subidy = {
+        enabled = true;
+        url = "https://airsonic.7596ff.com";
+        username = "somasis";
       };
 
       # beets = {
       #   hostname = "127.0.0.1";
       #   port = 8337;
       # };
+    };
+
+    extraConfigFiles = [ "${xdgRuntimeDir}/pass-mopidy-subidy.ini" ];
+  };
+
+  systemd.user.services."pass-mopidy-subidy" = {
+    Unit = {
+      Description = "Authenticate `mopidy-subidy` using `pass`";
+      PartOf = [ "default.target" ];
+
+      Before = [ "mopidy.service" ];
+
+      After = [ "gpg-agent.service" ];
+    };
+    Install.WantedBy = [ "default.target" "mopidy.service" ];
+
+    Service = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+
+      ExecStart = [ "${pass-mopidy-subidy}/bin/pass-mopidy-subidy spinoza.7596ff.com/airsonic/somasis" ];
+      ExecStop = [ "${pkgs.coreutils}/bin/rm -f %t/pass-mopidy-subidy.ini" ];
     };
   };
 }
