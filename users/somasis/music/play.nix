450217fe07a65a51e78a8412daad4b77c5daeb2f
Kylie McClain <kylie@somas.is>
2022-11-26 17:32:34 -0500

    music/play: use mpdscribble

--- users/somasis/music/play.nix
+++ users/somasis/music/play.nix
@@ -6,6 +6,43 @@
 }:
 let
   xdgRuntimeDir = "/run/user/${toString nixosConfig.users.users.${config.home.username}.uid}";
+
+  pass-mpdscribble = pkgs.writeShellApplication {
+    name = "pass-mpdscribble";
+    runtimeInputs = [
+      config.programs.password-store.package
+    ];
+
+    text = ''
+      cat <<EOF
+      log = -
+      host = ${xdgRuntimeDir}/mpd/socket
+      verbose = 2
+
+      [last.fm]
+      url = https://post.audioscrobbler.com/
+      username = kyliesomasis
+      password = $(pass www/last.fm/kyliesomasis | tr -d '\n' | md5sum - | cut -d' ' -f1)
+      journal = ${config.xdg.cacheHome}/mpdscribble/last.fm.journal
+
+      [listenbrainz]
+      url = http://proxy.listenbrainz.org
+      username = Somasis
+      password = $(pass ${nixosConfig.networking.fqdn}/mpdscribble/listenbrainz.org)
+      journal = ${config.xdg.cacheHome}/mpdscribble/listenbrainz.journal
+      EOF
+    '';
+  };
+
+  mpdscribble = pkgs.symlinkJoin {
+    name = "mpdscribble";
+    paths = [ pkgs.mpdscribble pass-mpdscribble ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/mpdscribble \
+          --add-flags '--conf <(pass-mpdscribble)'
+    '';
+  };
 in
 {
   services.mpd = {
@@ -19,7 +56,21 @@ in
 
   home.sessionVariables.MPD_HOST = config.services.mpd.network.listenAddress;
 
-  home.packages = [ pkgs.mpris-scrobbler ];
+  home.packages = [ mpdscribble ];
+
+  systemd.user.services.mpdscribble = {
+    Unit = {
+      Description = pkgs.mpdscribble.meta.description;
+      PartOf = [ "default.target" ];
+      After = [ "mpd.service" ];
+    };
+    Install.WantedBy = [ "default.target" "mpd.service" ];
+
+    Service = {
+      Type = "simple";
+      ExecStart = [ "${mpdscribble}/bin/mpdscribble -D" ];
+    };
+  };
 
   services.mpdris2 = {
     enable = config.services.mpd.enable;
