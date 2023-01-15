604694825c94ba8c3188e9ae7b8cee924c7c9f02
Kylie McClain <kylie@somas.is>
2023-01-15 14:36:27 -0500

    WIP somasis/mouse: switch to unclutter

--- users/somasis/desktop/mouse.nix
+++ users/somasis/desktop/mouse.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }: {
-  systemd.user.services.xbanish = {
-    Unit = {
-      Description = "Hide the mouse pointer when typing";
-      PartOf = [ "graphical-session.target" ];
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-
-    Service = {
-      ExecStart = "${pkgs.xbanish}/bin/xbanish -i all";
-      Restart = "always";
-    };
+  services.unclutter = {
+    enable = true;
+    timeout = 5;
+    extraOptions = [ "exclude-root" "ignore-scrolling" ];
   };
+
+  # systemd.user.services.xbanish = {
+  #   Unit = {
+  #     Description = "Hide the mouse pointer when typing";
+  #     PartOf = [ "graphical-session.target" ];
+  #   };
+  #   Install.WantedBy = [ "graphical-session.target" ];
+
+  #   Service = {
+  #     ExecStart = "${pkgs.xbanish}/bin/xbanish -i all";
+  #     Restart = "always";
+  #   };
+  # };
 }
