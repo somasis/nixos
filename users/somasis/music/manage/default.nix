6f94ac387beac3224352776b658be324707ff33b
Kylie McClain <kylie@somas.is>
2023-02-05 20:29:15 -0500

    music/manage: don't depend on nixpkgs with my PR

--- users/somasis/music/manage/default.nix
+++ users/somasis/music/manage/default.nix
@@ -189,13 +189,55 @@ let
   #     })
   #   { beets = pkgs.beetsPackages.beets-minimal; });
 
+  # TODO Wait for merge https://github.com/NixOS/nixpkgs/pull/203544
+  originquery = pkgs.callPackage
+    (
+      { lib, fetchFromGitHub, beets, python3Packages }:
+
+      python3Packages.buildPythonApplication rec {
+        pname = "beets-originquery";
+        version = "1.0.2";
+
+        src = fetchFromGitHub {
+          repo = pname;
+          owner = "x1ppy";
+          rev = version;
+          hash = "sha256-32S8Ik6rzw6kx69o9G/v7rVsVzGA1qv5pHegYDmTW68=";
+        };
+
+        nativeBuildInputs = [ beets ];
+
+        propagatedBuildInputs = with python3Packages; [
+          confuse
+          jsonpath_rw
+          pyyaml
+        ];
+
+        meta = with lib; {
+          description = "Integrate origin metadata (origin.txt) into beets' MusicBrainz queries";
+          homepage = "https://github.com/x1ppy/beets-originquery";
+          maintainers = with maintainers; [ somasis ];
+          license = licenses.unfree; # <https://github.com/x1ppy/beets-originquery/issues/3>
+          inherit (beets.meta) platforms;
+        };
+      }
+    )
+    { beets = pkgs.beetsPackages.beets-minimal; }
+  ;
+
   beets = (pkgs.beets.override {
     pluginOverrides = {
       # beetcamp = { enable = true; propagatedBuildInputs = [ beetcamp ]; };
       # fetchartist = { enable = true; propagatedBuildInputs = [ beets-fetchartist ]; };
       extrafiles = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.extrafiles ]; };
       noimport = { enable = true; propagatedBuildInputs = [ beets-noimport ]; };
-      originquery = { enable = true; propagatedBuildInputs = [ pkgs.beetsPackages.originquery ]; };
+      originquery = {
+        enable = true;
+        propagatedBuildInputs = [
+          # pkgs.beetsPackages.originquery
+          originquery
+        ];
+      };
     };
   });
 in
