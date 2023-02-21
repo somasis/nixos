2edb921727ae2e7923f768223e6257d41be8fbc9
Kylie McClain <kylie@somas.is>
2023-02-21 11:28:33 -0500

    ilo/print: enable cups-pdf

--- hosts/ilo.somas.is/hardware/print.nix
+++ hosts/ilo.somas.is/hardware/print.nix
@@ -1,17 +1,14 @@
 { pkgs, ... }: {
   services.printing = {
     enable = true;
-
     browsing = true;
-
-    drivers = [
-      pkgs.gutenprint
-      # pkgs.brlaser
-      # pkgs.hplip
-    ];
+    cups-pdf.enable = true;
+    drivers = [ pkgs.gutenprint ];
   };
 
   # Necessary for discovering printers on the network.
+  # CUPS doesn't use systemd-resolved for discovery.
+  # <https://github.com/apple/cups/issues/5452>
   services.avahi = {
     enable = true;
     nssmdns = true;
