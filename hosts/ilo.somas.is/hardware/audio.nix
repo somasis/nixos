f1d3518304817b914f37bbac8ab3796aa10edab6
Kylie McClain <kylie@somas.is>
2023-02-21 11:28:07 -0500

    ilo/audio: back to pulseaudio
    Pipewire had some Bluetooth issues, it would only let me use my
    headphones in headset mode?

--- hosts/ilo.somas.is/hardware/audio.nix
+++ hosts/ilo.somas.is/hardware/audio.nix
@@ -1,4 +1,4 @@
-# { pkgs, ... }:
+{ pkgs, ... }:
 {
   # Enable ALSA and preserve the mixer state across boots.
   sound.enable = true;
@@ -7,18 +7,18 @@
   # Necessary for realtime usage.
   security.rtkit.enable = true;
 
-  # hardware.pulseaudio = {
-  #   enable = true;
-  #   support32Bit = true;
-  #   # package = pkgs.pulseaudioFull;
-  # };
-
-  services.pipewire = {
+  hardware.pulseaudio = {
     enable = true;
-    audio.enable = true;
-
-    alsa.enable = true;
-    pulse.enable = true;
-    jack.enable = true;
+    support32Bit = true;
+    package = pkgs.pulseaudioFull;
   };
+
+  # services.pipewire = {
+  #   enable = true;
+  #   audio.enable = true;
+
+  #   alsa.enable = true;
+  #   pulse.enable = true;
+  #   jack.enable = true;
+  # };
 }
