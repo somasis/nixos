bfa2dd70ac209508a986e75ac751fcd8fb957d9c
Kylie McClain <kylie@somas.is>
2023-02-23 14:08:04 -0500

    browser: fill out accept language list

--- users/somasis/desktop/browser/default.nix
+++ users/somasis/desktop/browser/default.nix
@@ -193,7 +193,7 @@ in
 
       # Languages preferences.
       spellcheck.languages = [ "en-US" "en-AU" "en-GB" "es-ES" ];
-      content.headers.accept_language = lib.concatStringsSep "," (lib.flatten [ "tok" "en;q=0.9" ]);
+      content.headers.accept_language = lib.concatStringsSep "," [ "tok;q=0.9" "en-US;q=0.8" "en;q=0.7" "es;q=0.6" ];
 
       zoom = {
         # This will be unnecessary if I ever start using Wayland and don't
