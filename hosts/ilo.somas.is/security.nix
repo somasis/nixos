95366916e3b6359336e4d7c5e3800f2a80a39b72
Kylie McClain <kylie@somas.is>
2023-03-14 00:43:59 -0400

    ilo/security: add doas completion

--- hosts/ilo.somas.is/security.nix
+++ hosts/ilo.somas.is/security.nix
@@ -27,4 +27,11 @@
 
   # Always automatically recover from kernel panics by rebooting in 60 seconds
   boot.kernelParams = [ "panic=60" ];
+
+  programs.bash.interactiveShellInit = ''
+    . ${pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/Duncaen/OpenDoas/51f126a9a56cda5a291d5652b0685967133d7b90/doas.completion";
+      hash = "sha256-SiByFXX4DOA1FKn7M7r/+3REsf7lfs1oJm/h0a2pdFI=";
+    }}
+  '';
 }
