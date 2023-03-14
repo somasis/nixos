1e6c14a94c754aa825631fcd2208bba1494b577c
Kylie McClain <kylie@somas.is>
2023-03-14 00:20:31 -0400

    browser/search: sort

--- users/somasis/desktop/browser/search.nix
+++ users/somasis/desktop/browser/search.nix
@@ -11,24 +11,29 @@
     "!gi" = "https://google.com/search?tbm=isch&source=hp&q={}";
     "!yt" = "https://www.youtube.com/results?search_query={}";
 
+    "!appstate" = "https://gb1.appstate.edu/search?q={}";
+    "!apppeople" = "https://search.appstate.edu/search.php?last={}&type=all";
+    "!atsd" = "https://jira.appstate.edu/browse/ATSD-{}";
+    "!tss" = "https://jira.appstate.edu/secure/QuickSearch.jspa?searchString={}";
+    "!dell" = "https://www.dell.com/support/home/en-us/product-support/servicetag/{}";
+
     "!libgen" = "http://libgen.rs/index.php?req={}";
     "!bookfinder" = "https://www.bookfinder.com/search/?keywords={}";
     "!abebooks" = "https://www.abebooks.com/servlet/SearchResults?kn={}";
-
-    "!appstate" = "https://gb1.appstate.edu/search?q={}";
-    "!apppeople" = "https://search.appstate.edu/search.php?last={}&type=all";
-    "!library" = "https://login.proxy006.nclive.org/login?qurl=http%3A%2F%2Fsearch.ebscohost.com%2Flogin.aspx%3Fdirect%3Dtrue%26site%3Deds-live%26scope%3Dsite%26group%3Dmain%26profile%3Deds%26authtime%3Dcookie%2Cip%2Cuid%26bQuery%3D{quoted}";
-    "!scholar" = "https://login.proxy006.nclive.org/login?qurl=https%3A%2F%2Fscholar.google.com%2Fscholar%3Fhl%3Den%26q%3D{quoted}%26btnG%3DSearch";
+    "!plato" = "https://plato.stanford.edu/search/searcher.py?query={}";
+    "!iep" = lib.replaceStrings [ "{}" ] [ "site:https://iep.utm.edu+{}" ] DEFAULT;
+    "!doi" = "https://doi.org/{unquoted}";
 
     "!pkg" = "https://parcelsapp.com/en/tracking/{}";
 
     "!discogs" = "https://www.discogs.com/search/?q={}";
 
-    "!mbartist" = "https://musicbrainz.org/search?query={}&type=artist&method=advanced";
-    "!mbrecording" = "https://musicbrainz.org/search?query={}&type=recording&method=advanced";
-    "!mbrelease" = "https://musicbrainz.org/search?query={}&type=release&method=advanced";
-    "!mbreleasegroup" = "https://musicbrainz.org/search?query={}&type=release_group&method=advanced";
-    "!mbwork" = "https://musicbrainz.org/search?query={}&type=work&method=advanced";
+    "!mbartist" = "https://musicbrainz.org/search?query={}&type=artist";
+    "!mbrecording" = "https://musicbrainz.org/search?query={}&type=recording";
+    "!mbrelease" = "https://musicbrainz.org/search?query={}&type=release";
+    "!mbreleasegroup" = "https://musicbrainz.org/search?query={}&type=release_group";
+    "!mbseries" = "https://musicbrainz.org/search?query={}&type=series";
+    "!mbwork" = "https://musicbrainz.org/search?query={}&type=work";
 
     "!letterboxd" = "https://letterboxd.com/search/{}/";
     "!imdb" = "https://www.imdb.com/find/?s=all&q={}";
@@ -70,9 +75,6 @@
     "!tok" = "https://wikipesija.org/wiki/nimi:{}";
     "!linku" = "https://lipu-linku.github.io/?q={}";
 
-    "!plato" = "https://plato.stanford.edu/search/searcher.py?query={}";
-    "!iep" = lib.replaceStrings [ "{}" ] [ "site:https://iep.utm.edu+{}" ] DEFAULT;
-
     "!archman" = "https://man.archlinux.org/search?q={}";
     "!archpkgs" = "https://archlinux.org/packages/?sort=&q={}";
     "!archwiki" = "https://wiki.archlinux.org/index.php?title=Special%3ASearch&search={}";
@@ -85,9 +87,5 @@
     "!emugen" = "https://emulation.gametechwiki.com/index.php?title=Special%3ASearch&search={}";
     "!redump" = "http://redump.org/discs/quicksearch/{quoted}";
     "!vimm" = "https://vimm.net/vault/?p=list&q={}";
-
-    "!atsd" = "https://jira.appstate.edu/browse/ATSD-{}";
-    "!tss" = "https://jira.appstate.edu/secure/QuickSearch.jspa?searchString={}";
-    "!dell" = "https://www.dell.com/support/home/en-us/product-support/servicetag/{}";
   };
 }
