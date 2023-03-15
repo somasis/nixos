f455d820e422065d2ff9f11c66e4e91974eccff2
Kylie McClain <kylie@somas.is>
2023-03-15 19:16:24 -0400

    browser: add greasemonkey script

--- users/somasis/desktop/browser/greasemonkey.nix
+++ users/somasis/desktop/browser/greasemonkey.nix
@@ -87,6 +87,7 @@ in
     (pkgs.fetchurl { hash = "sha256-azHAQKmNxAcnyc7l08oW9X6DuMqAblFGPwD8T9DsrSs="; url = "https://greasyfork.org/scripts/32635-disable-google-search-result-url-redirector/code/Disable%20Google%20Search%20Result%20URL%20Redirector.user.js"; })
     (pkgs.fetchurl { hash = "sha256-Bb1QsU6R9xU718hRskGbuwNO7rrhuV7S1gvKtC9SlL0="; url = "https://greasyfork.org/scripts/37166-add-site-search-links-to-google-search-result/code/Add%20Site%20Search%20Links%20To%20Google%20Search%20Result.user.js"; })
     (pkgs.fetchurl { hash = "sha256-5C7No5dYcYfWMY+DwciMeBmkdE/wnplu5fxk4q7OFZc="; url = "https://greasyfork.org/scripts/382039-speed-up-google-captcha/code/Speed%20up%20Google%20Captcha.user.js"; })
+    (pkgs.fetchurl { hash = "sha256-O7xInRO9H+AhG8Y6Ky5IDVJK++jzMHggSeq5X7/Je1A="; url = "https://raw.githubusercontent.com/jmlntw/google-search-sidebar/v0.4.1/dist/google-search-sidebar.user.js"; })
 
     # images.google.com
     (pkgs.fetchurl { hash = "sha256-lNGCdMjf5RsR/70T81VaW2S5w71PSK4pHwsaBTL6Gqg="; url = "https://greasyfork.org/scripts/29420-google-dwimages/code/Google%20DWIMages.user.js"; })
