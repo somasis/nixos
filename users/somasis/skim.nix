56d9f8b374767999dd8f11d7aa1e3b18b25c9811
Kylie McClain <kylie@somas.is>
2023-04-16 01:19:27 -0400

    somasis/skim: init

--- users/somasis/skim.nix
+++ users/somasis/skim.nix
@@ -1,7 +1,52 @@
+{ lib
+, config
+, ...
+}:
+let
+  skimList = lib.concatStringsSep ",";
+  attrsToSkimList = a: skimList (lib.mapAttrsToList (n: v: "${n}:${builtins.toString v}") a);
+in
 {
   programs.skim = {
     enable = true;
-    defaultOptions = [ "--exact" "--layout reverse" "--prompt '/ '" "--cmd-prompt '∴ '" ];
+    defaultOptions =
+      let
+        binds = attrsToSkimList { ctrl-h = "unix-word-rubout"; };
+
+        colors = attrsToSkimList {
+          bg = -1;
+          fg = -1;
+
+          matched_bg = -1;
+          matched = 1;
+
+          current_bg = "#ffffff";
+          current = config.xresources.properties."*colorAccent";
+
+          current_match_bg = 1;
+          current_match = -1;
+
+          query_bg = -1;
+          query = -1;
+
+          info = 0;
+          border = 0;
+          prompt = 9;
+          pointer = config.xresources.properties."*colorAccent";
+          marker = 9;
+          spinner = 2;
+          header = 12;
+        };
+      in
+      [
+        "--inline-info"
+
+        "--prompt '/ '"
+        "--cmd-prompt '∴ '"
+
+        "--color=bw,${colors}"
+        "--bind=${binds}"
+      ];
   };
 
   programs.kakoune.extraConfig = ''
