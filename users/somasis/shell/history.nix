f279d2476f53bd64d62e565c78575ca66636d7a9
Kylie McClain <kylie@somas.is>
2023-04-16 01:19:04 -0400

    history: fix time prefix

--- users/somasis/shell/history.nix
+++ users/somasis/shell/history.nix
@@ -4,6 +4,6 @@
     historyFileSize = lib.mkDefault 0;
     historyControl = [ "ignorespace" "ignoredups" ];
 
-    sessionVariables.HISTTIMEFORMAT = "%Y-%m-%dT%H:%M:%S%z";
+    sessionVariables.HISTTIMEFORMAT = "%Y-%m-%dT%H:%M:%S ";
   };
 }
