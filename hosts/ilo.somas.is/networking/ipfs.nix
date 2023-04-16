62e3a025bac9eb38700a056e4061c1507b0a643a
Kylie McClain <kylie@somas.is>
2023-04-16 01:20:51 -0400

    ipfs: init

--- hosts/ilo.somas.is/networking/ipfs.nix
+++ hosts/ilo.somas.is/networking/ipfs.nix
@@ -1,9 +1,33 @@
 { pkgs, ... }: {
   services.kubo = {
     enable = true;
-    enableGC = true;
     emptyRepo = true;
 
     startWhenNeeded = true;
+
+    settings = {
+      # Apply some of the settings from the "lowpower" profile
+      AutoNAT.ServiceMode = "disabled";
+      Reprovider.Interval = 0;
+
+      Swarm.ConnMgr = {
+        HighWater = 20;
+        LowWater = 5;
+        GracePeriod = "1m0s";
+      };
+
+      Datastore = {
+        StorageMax = "2GB";
+        GCPeriod = "24h";
+      };
+    };
   };
+
+  systemd.services.ipfs.serviceConfig.stopWhenUnneeded = true;
+
+  environment.persistence."/persist".directories = [{
+    directory = "/var/lib/ipfs";
+    user = "ipfs";
+    group = "ipfs";
+  }];
 }
