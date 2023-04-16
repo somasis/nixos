1b35ccafcd207741f24096de3da433b2580c9a76
Kylie McClain <kylie@somas.is>
2023-04-15 23:23:33 -0400

    mouse: add touchpad gestures for emulating buttons

--- users/somasis/desktop/mouse.nix
+++ users/somasis/desktop/mouse.nix
@@ -5,6 +5,23 @@
     extraOptions = [ "exclude-root" "ignore-scrolling" ];
   };
 
+  services.fusuma = {
+    enable = true;
+    extraPackages = [ pkgs.coreutils pkgs.xdotool ];
+
+    settings = {
+      swipe = {
+        # Emulate Back/Forward mouse buttons with three-finger swipe left/right
+        "3".right.command = "xdotool click --clearmodifiers 8";
+        "3".left.command = "xdotool click --clearmodifiers 9";
+
+        # Send Home/End keys with three finger swipe up/down
+        # "3".down.command = "xdotool key --clearmodifiers Home";
+        # "3".up.command = "xdotool key --clearmodifiers End";
+      };
+    };
+  };
+
   # systemd.user.services.xbanish = {
   #   Unit = {
   #     Description = "Hide the mouse pointer when typing";
