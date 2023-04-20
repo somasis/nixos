8f2271d7a4fc7b01b312608b30c50fed07146de6
Kylie McClain <kylie@somas.is>
2023-04-20 11:46:53 -0400

    ilo/console: use kmscon

--- hosts/ilo.somas.is/console.nix
+++ hosts/ilo.somas.is/console.nix
@@ -21,4 +21,43 @@
       "e0d6d6" # color15: bright grey
     ];
   };
+
+  services.kmscon = {
+    enable = true;
+
+    hwRender = false;
+
+    extraConfig = ''
+      vt=2
+      sb-size=10000
+      font-size=11
+      font-dpi=144
+
+      palette=custom
+      palette-background=47,52,63
+      palette-foreground=224,234,240
+      palette-black=117,95,95
+      palette-red=207,67,66
+      palette-green=172,192,68
+      palette-yellow=239,147,36
+      palette-blue=67,141,197
+      palette-magenta=197,77,122
+      palette-cyan=73,155,175
+      palette-light-grey=216,199,199
+      palette-dark-grey=147,116,116
+      palette-light-red=254,98,98
+      palette-light-green=196,233,120
+      palette-light-yellow=248,220,60
+      palette-light-blue=150,199,236
+      palette-light-magenta=249,124,172
+      palette-light-cyan=48,208,242
+      palette-white=224,214,214
+    '';
+
+    fonts = [
+      { name = "Iosevka Term"; package = pkgs.iosevka-bin; }
+      # { name = "Sarasa Term CL"; package = pkgs.sarasa-gothic; }
+      # { name = "Twitter Color Emoji"; package = pkgs.twitter-color-emoji; }
+    ];
+  };
 }
