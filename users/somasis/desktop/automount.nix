2acd8566abcdd4dd9333de811e977cb12311a285
Kylie McClain <kylie@somas.is>
2023-06-26 14:36:48 -0400

    desktop/automount: more options, add password prompt

--- users/somasis/desktop/automount.nix
+++ users/somasis/desktop/automount.nix
@@ -1,8 +1,37 @@
-{ nixosConfig, ... }:
-assert nixosConfig.services.udisks2.enable;
+{ config
+, osConfig
+, pkgs
+, ...
+}:
+let
+  dmenu-password = pkgs.writeShellScript "dmenu-password" ''
+    dmenu -p "password [$1]" -P </dev/null
+  '';
+in
 {
   services.udiskie = {
-    enable = true;
-    tray = "never";
+    inherit (osConfig.services.udisks2) enable;
+
+    automount = false;
+    notify = true;
+    tray = "auto";
+
+    settings = {
+      program_options = {
+        menu = "flat";
+
+        password_prompt = [ dmenu-password "{device_presentation}" ];
+        password_cache = 60;
+      };
+
+      notifications.timeout = -1;
+      notification_actions = {
+        device_added = [ "mount" "detach" ];
+        device_mounted = [ "browse" "unmount" ];
+        device_unmounted = [ "mount" "detach" ];
+      };
+    };
   };
+
+  home.packages = [ pkgs.udiskie ];
 }
