24e4e798fc0f10eafb721e274665ea92ec79d440
Kylie McClain <kylie@somas.is>
2023-06-26 14:37:25 -0400

    commands/sonapona: add stuff for scraping twitter

--- users/somasis/commands/sonapona.nix
+++ users/somasis/commands/sonapona.nix
@@ -1,6 +1,100 @@
-{ config, pkgs, ... }:
+{ config
+, lib
+, pkgs
+, ...
+}:
 let
   directory = "share/sonapona";
+
+  click-config-file = with pkgs.python3Packages;
+    buildPythonPackage rec {
+      pname = "click-config-file";
+      version = "0.6.0";
+
+      src = fetchPypi {
+        pname = "click_config_file";
+        version = "0.6.0";
+        hash = "sha256-3tbsGnPEEoByfsnAYDHpKc3YpZRr8PmcDD2zpxeT1RU=";
+      };
+
+      propagatedBuildInputs = [
+        click
+        configobj
+      ];
+
+      checkInputs = [
+        pytestCheckHook
+        pytest-runner
+        twine
+      ];
+
+      postPatch = ''
+        substituteInPlace setup.py \
+          --replace "'pytest-runner'" ""
+      '';
+
+      doCheck = false;
+      pythonImportsCheck = [ "click_config_file" ];
+
+      meta = with lib; {
+        description = "Add support for configuration files to Click applications";
+        homepage = "https://github.com/phha/click_config_file";
+        license = licenses.mit;
+      };
+    }
+  ;
+
+  twarc = with pkgs.python3Packages;
+    buildPythonApplication rec {
+      pname = "twarc";
+      version = "2.14.0";
+
+      src = fetchPypi {
+        inherit pname version;
+        hash = "sha256-+o7jBS2LlngjG+qV0b3Lq7OWjTXFao0fztyJgujGamY=";
+      };
+
+      propagatedBuildInputs = [
+        click
+        click-config-file
+        click-plugins
+        humanize
+        python-dateutil
+        requests-oauthlib
+        tqdm
+
+        setuptools # needs pkg_resources
+      ];
+
+      checkInputs = [
+        pytestCheckHook
+        pytest-runner
+
+        black
+        pytest-black
+        pytest-dotenv
+
+        pytz
+        tomli
+      ];
+
+      postPatch = ''
+        substituteInPlace setup.py \
+          --replace '"pytest-runner"' ""
+      '';
+
+      preBuild = "export HOME=$(mktemp -d)";
+
+      doCheck = false;
+      pythonImportsCheck = [ "twarc" ];
+
+      meta = with lib; {
+        description = "Archive tweets from the command line";
+        homepage = "https://github.com/docnow/twarc";
+        license = licenses.mit;
+      };
+    }
+  ;
 in
 {
   home.packages = [
@@ -15,15 +109,18 @@ in
       ];
 
       text = ''
-        bfs -H "$HOME/${directory}" \
+        bfs -H "$HOME/"${lib.escapeShellArg directory} \
             -mindepth 2 \
             -type f \
             ! -executable \
-            "$@" -exec shuf -n 1 -e {} + \
+            -print0 \
+            | shuf -z -n 1 \
             | xe fold -w 80 -s \
             | sed 's/ *$//'
       '';
     })
+
+    twarc
   ];
 
   persist.directories = [{
