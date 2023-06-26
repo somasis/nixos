f647b7b136d5a418b60ad7bb3bf3184df748e40c
Kylie McClain <kylie@somas.is>
2023-06-26 13:55:49 -0400

    commaList: escape input

--- modules/lib.nix
+++ modules/lib.nix
@@ -24,7 +24,7 @@ with lib;
 
     mkPathSafeName = replaceStrings [ "@" ":" "\\" "[" "]" ] [ "-" "-" "-" "" "" ];
 
-    commaList = concatStringsSep ",";
+    commaList = concatMapStringsSep "," (lib.escape [ "," ]);
 
     # testCase -> TEST_CASE
     camelCaseToScreamingSnakeCase = x:
