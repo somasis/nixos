f0b574e33004b9a846016d904d5fdf12a9b5cac7
Kylie McClain <kylie@somas.is>
2023-06-26 13:59:15 -0400

    dmenu-run: filter out hidden programs in $PATH

--- pkgs/dmenu-run/dmenu-run.bash
+++ pkgs/dmenu-run/dmenu-run.bash
@@ -10,7 +10,10 @@ choice=$(
         IFS=:
         # We want the $PATH to be split here.
         # shellcheck disable=SC2086
-        find ${PATH} ! -type d -executable 2>/dev/null \
+        find ${PATH} \
+            ! -name '.*' \
+            ! -type d \
+            -executable 2>/dev/null \
             | sed 's@.*/@@' \
             | sort "${DMENU_RUN_HISTORY}" - \
             | cat "${DMENU_RUN_HISTORY}" - 2>/dev/null
@@ -27,7 +30,7 @@ ${SHELL:-sh} -x - <<<"${choice}" &
 
 cat - "${DMENU_RUN_HISTORY}" <<<"${choice}" \
     | head -n 24 \
-    | grep -v "^\s*$" \
+    | grep -v -e "^\s*$" -e '^\..*' \
     | uq \
     | while read -r line; do
         base=${line%% *}
