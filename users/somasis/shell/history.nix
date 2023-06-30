1685175f97a5a6b387ef2952ac213687771c7e81
Kylie McClain <kylie@somas.is>
2023-06-30 14:20:10 -0400

    history

--- users/somasis/shell/history.nix
+++ users/somasis/shell/history.nix
@@ -2,7 +2,10 @@
   programs.bash = {
     historyFile = lib.mkDefault "/dev/null";
     historyFileSize = lib.mkDefault 0;
-    historyControl = [ "ignoredups" "ignorespace" ];
+    historyControl = [ "ignoredups" ]
+      # kitty's shell integration will complain if "ignorespace" is in $HISTCONTROL.
+      ++ lib.optional (!config.programs.kitty.enable) "ignorespace"
+    ;
 
     sessionVariables.HISTTIMEFORMAT = "%Y-%m-%dT%H:%M:%S ";
   };
