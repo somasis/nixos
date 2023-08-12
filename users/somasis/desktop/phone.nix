332bcba93a96e4cb1847bb2c25c042ed04f08d5b
Kylie McClain <kylie@somas.is>
2023-08-12 19:48:15 -0400

    phone: tweaks

--- users/somasis/desktop/phone.nix
+++ users/somasis/desktop/phone.nix
@@ -5,38 +5,33 @@
 , ...
 }:
 let
-  kdeconnectShare = pkgs.writeShellApplication {
-    name = "kdeconnect-share";
-
-    runtimeInputs = [
-      pkgs.plasma5Packages.kdeconnect-kde
-      pkgs.xe
-    ];
-
-    text = ''
-      while [ $# -gt 0 ]; do
-          kdeconnect-cli -a --id-only \
-              | xe -j0 kdeconnect-cli -d {} --share "$1"
-          shift
-      done
-    '';
-  };
+  kdeconnectShare = pkgs.writeShellScript "kdeconnect-share" ''
+    PATH=${lib.makeBinPath [ pkgs.plasma5Packages.kdeconnect-kde pkgs.xe ]}:"$PATH"
+    while [ $# -gt 0 ]; do
+        kdeconnect-cli -a --id-only \
+            | xe -j0 kdeconnect-cli -d {} --share "$1"
+        shift
+    done
+  '';
 in
 {
-  services.kdeconnect.enable = true;
-
-  xdg.configFile = {
-    # "kdeconnect/.keep".source = builtins.toFile "keep" "";
-
-    "kdeconnect/config".text = lib.generators.toINI { } {
-      General.name = "${config.home.username}@${osConfig.networking.fqdnOrHostName}";
-    };
+  services.kdeconnect = {
+    enable = true;
+    indicator = true;
   };
 
-  persist.directories = [{ method = "symlink"; directory = "etc/kdeconnect"; }];
+  xdg.configFile."kdeconnect/config".text = lib.generators.toINI { } {
+    General.name = "${config.home.username}@${osConfig.networking.fqdnOrHostName}";
+  };
+
+  persist.directories = [{
+    # bindfs must be used because of the configFile we're making in the directory
+    method = "bindfs";
+    directory = "etc/kdeconnect";
+  }];
 
   programs.qutebrowser = {
     aliases.kdeconnect = "spawn -u ${kdeconnectShare}";
-    keyBindings.normal."zk" = "kdeconnect";
+    keyBindings.normal."zk" = "kdeconnect {url}";
   };
 }
