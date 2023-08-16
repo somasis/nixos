e0862e8e4d55675039cf88d4fce432f46fecd2b5
Kylie McClain <kylie@somas.is>
2023-08-16 11:07:23 -0400

    music/manage: use beets-alternatives

--- users/somasis/music/manage/convert.nix
+++ users/somasis/music/manage/convert.nix
@@ -7,10 +7,15 @@
 }: {
   programs.beets.settings = rec {
     plugins = [ "convert" "alternatives" ];
+
     alternatives.lossy = {
-      directory = "${config.xdg.userDirs.music}/alternatives_test";
+      directory = "${config.xdg.userDirs.music}/lossy";
       formats = [ "opus" ];
+
       query = "";
+
+      paths.default = "%if{$mb_albumartistid,$mb_albumartistid/}%if{$mb_albumid,$mb_albumid/}%ifdef{mb_releasetrackid,%ifdef{mb_trackid}}";
+
       removable = false;
     };
 
@@ -33,100 +38,5 @@
 
       paths.default = "%if{$mb_albumartistid,$mb_albumartistid/}%if{$mb_albumid,$mb_albumid/}%ifdef{mb_releasetrackid,%ifdef{mb_trackid}}";
     };
-
-    # hook.hooks = [
-    #   { event = "after_write"; command = "touch ${convert.dest}/.updated"; }
-    # ];
   };
-
-  # TODO make managing of the converted music library automatic
-  # programs.beets.settings.hook.hooks =
-  #   let
-  #     # Propagate item changes in the main library to the converted library
-  #     hookPushItemToConverted = (pkgs.writeShellScript "beet-hook-push-item-to-converted" ''
-  #       ${lib.toShellVar "path_converted" convert.dest}
-  #       ${lib.toShellVar "extension_converted" convert.formats."${convert.format}".extension}
-  #       ${lib.toShellVar "format_converted" convert.paths.default}
-
-  #       converted_path=$(beet list -f "$path_converted/$format_converted.$extension_converted" "path:$1")
-  #       converted_album_path=$(dirname "$converted_path")
-
-  #       rm -f "$converted_path"
-  #       find "$converted_album_path" -name 'cover.*' -delete
-  #       beet convert -ay "path:$1"
-  #     '');
-
-  #     # Remove items from the converted library when they're removed from the main library
-  #     hookRemoveItemFromConverted = (pkgs.writeShellScript "beet-hook-remove-item-from-converted" ''
-  #       ${lib.toShellVar "path_converted" convert.dest}
-  #       ${lib.toShellVar "extension_converted" convert.formats."${convert.format}".extension}
-  #       ${lib.toShellVar "format_converted" convert.paths.default}
-
-  #       converted_path=$(beet list -f "$path_converted/$format_converted.$extension_converted" "path:$1")
-  #       rm -f "$converted_path"
-  #     '');
-
-  #     # Remove albums from the converted library when they're removed from the main library
-  #     hookRemoveAlbumFromConverted = (pkgs.writeShellScript "beet-hook-remove-album-from-converted" ''
-  #       ${lib.toShellVar "path_converted" convert.dest}
-  #       ${lib.toShellVar "extension_converted" convert.formats."${convert.format}".extension}
-  #       ${lib.toShellVar "format_converted" convert.paths.default}
-
-  #       converted_path=$(beet list -f "$path_converted/$format_converted.$extension_converted" "path:$1")
-  #       converted_album_path=$(dirname "$converted_path")
-  #       rm -r "$converted_album_path"
-  #     '');
-  #   in
-  #   [
-  #     { event = "after_write"; command = "${hookPushItemToConverted} {item.path}"; }
-  #     { event = "item_removed"; command = "${hookRemoveItemFromConverted} {item.path}"; }
-  #     { event = "album_removed"; command = "${hookRemoveAlbumFromConverted} {album.path}"; }
-  #   ];
-
-  home.packages = [
-    (pkgs.writeShellApplication {
-      name = "beet-sync-converted";
-
-      runtimeInputs = [
-        config.programs.beets.package
-        pkgs.coreutils
-        pkgs.findutils
-        pkgs.gnugrep
-        pkgs.gnused
-        pkgs.xe
-      ];
-
-      text = ''
-        # shellcheck disable=SC2016
-        {
-            ${lib.toShellVar "dest" config.programs.beets.settings.convert.dest}
-            ${lib.toShellVar "path_format" config.programs.beets.settings.convert.paths.default}
-        }
-
-        canonicalize() {
-            LC_COLLATE=C sort | sed -E 's:/+$::'
-        }
-
-        expected_library=$(
-            beet ls -a -f "$dest/$path_format" | canonicalize
-        )
-
-        converted_library=$(
-            find "$dest" \
-                ! -name '.*' \
-                -type d \
-                -links 2 \
-                | canonicalize
-        )
-
-        diff --color -u <(printf '%s\n' "$converted_library") <(printf '%s\n' "$expected_library")
-
-        # Get only the paths that are present in the converted library,
-        # but not in the list of expected paths, and then delete them
-        grep -vF -f <(printf '%s\n' "$expected_library") <(printf '%s\n' "$converted_library") | xe -F -N0 rm -vr
-
-        beet convert -y -a >/dev/null
-      '';
-    })
-  ];
 }
--- users/somasis/music/manage/ripping.nix
+++ users/somasis/music/manage/ripping.nix
@@ -33,7 +33,7 @@
       };
 
       "whipper.cd.rip" = {
-        working_directories = "${config.xdg.userDirs.music}/source/rip";
+        working_directory = "${config.xdg.userDirs.music}/source/rip";
 
         track_template = "%A - %d (%y)/%t - %a - %n";
         disc_template = "%A - %d (%y)/%A - %d (%y) (disc %N)";
