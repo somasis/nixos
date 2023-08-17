a7c25e37f16d18d1532cbabae98264d0dd83a049
Kylie McClain <kylie@somas.is>
2023-08-17 12:40:17 -0400

    audio: use pipewire

--- hosts/ilo.somas.is/hardware/audio.nix
+++ hosts/ilo.somas.is/hardware/audio.nix
@@ -6,22 +6,45 @@
   # Necessary for realtime usage.
   security.rtkit.enable = true;
 
-  hardware.pulseaudio = {
+  services.pipewire = {
     enable = true;
-    support32Bit = true;
-    package = pkgs.pulseaudioFull;
+    audio.enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+    jack.enable = true;
   };
 
-  # services.pipewire = {
-  #   enable = true;
-  #   audio.enable = true;
+  # Sonos output functionality using AirPlay
+  # environment.etc."pipewire/pipewire.conf.d/network-audio.conf".text = ''
+  #   # <https://wiki.archlinux.org/title/PipeWire#Streaming_audio_to_an_AirPlay_receiver>
+  #   context.modules = [
+  #       {
+  #          name = libpipewire-module-raop-discover
+  #          args = { }
+  #       }
 
-  #   alsa = {
-  #     enable = true;
-  #     support32Bit = true;
-  #   };
+  #       # <https://wiki.archlinux.org/title/PipeWire#Sharing_audio_devices_with_computers_on_the_network>
+  #       {
+  #          name = libpipewire-module-rtp-session
+  #          args = { }
+  #       }
+  #       {
+  #          name = libpipewire-module-zeroconf-discover
+  #          args = { }
+  #       }
+  #   ]
 
-  #   pulse.enable = true;
-  #   jack.enable = true;
-  # };
+  #   # <https://wiki.archlinux.org/title/PipeWire#Sharing_audio_devices_with_computers_on_the_network>
+  #   context.exec = [
+  #       { path = "pactl" args = "load-module module-native-protocol-tcp" }
+  #   ]
+  # '';
+
+  # <https://wiki.archlinux.org/title/PipeWire#Streaming_audio_to_an_AirPlay_receiver>
+  # networking.firewall.allowedUDPPorts = [ 6001 6002 ];
 }
