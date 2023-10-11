c3d218fe9bf1cf58aa45495182a80c4bffcedd52
Kylie McClain <kylie@somas.is>
2023-10-11 00:41:25 -0400

    desktop/dates: improvements

--- users/somasis/desktop/dates.nix
+++ users/somasis/desktop/dates.nix
@@ -6,7 +6,15 @@
 }:
 let
   makeDatesCalendar = pkgs.writeShellScript "make-dates-calendar" ''
-    PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.dates pkgs.moreutils pkgs.rwc pkgs.snooze pkgs.wcal ]}:"$PATH"
+    PATH=${lib.makeBinPath [
+      pkgs.coreutils
+      pkgs.dates
+      pkgs.dateutils
+      pkgs.moreutils
+      pkgs.rwc
+      pkgs.snooze
+      pkgs.wcal
+    ]}:"$PATH"
 
     : "''${XDG_RUNTIME_DIR:=/run/user/$(id -u)}"
     runtime="$XDG_RUNTIME_DIR/dates-calendar"
@@ -14,15 +22,33 @@ let
     output="$runtime/output.txt"
     calendar="$runtime/calendar.txt"
     dates="$runtime/dates.txt"
+    events="$runtime/events.txt"
+    width="$runtime/width.txt"
+    height="$runtime/height.txt"
 
     mkdir -p "$runtime"
 
-    trap 'trap - TERM EXIT; rm -f "$calendar" "$dates" "$output"' EXIT INT TERM QUIT HUP
-    touch "$calendar" "$dates" "$output"
+    trap 'trap - TERM EXIT; rm -f "$output"' EXIT INT TERM QUIT HUP
+    touch "$output" "$calendar" "$dates" "$events" "$width" "$height"
+
+    generate_output() {
+        {
+            line_length=64
+            [ -s "$width" ] && [ "$(<"$width")" -ge 64 ] && line_length=$(<"$width")
+            paste -d ' ' "$calendar" "$dates" | tee >(wc -L > "$width")
+            cat "$events" | ifne ellipsis "$line_length"
+        } | ifne sponge "$output"
+        wc -l < "$output" > "$height"
+    }
+
+    if [ -e "$calendar" ] && [ "$(stat -c %Y "$calendar")" -lt "$(dateconv -f %s today)" ]; then
+        printf '...\n' > "$calendar"
+    fi
 
     {
         while [ -e "$calendar" ]; do
             wcal -i \
+                | head \
                 | sed -E \
                     -e 's/^([0-9]+) +([0-9A-Za-z]{3,}+)/\1\t\2\t/' \
                     -e '/^[0-9]+ +/ s/  +/\t\t/' \
@@ -36,7 +62,6 @@ let
                     -e 's/ $//' \
                     -e 's/\t/ /' \
                     -e '/^Wk/ s/^Wk  //' \
-                    -e '10q' \
                 | sponge "$calendar"
             snooze
         done
@@ -52,10 +77,18 @@ let
         done
     } &
 
-    while rwc -e "$calendar" "$dates" >/dev/null; do
-        paste "$calendar" "$dates" \
-            | tr '\t' ' ' \
-            | sponge "$output"
+    {
+        while [ -e "$events" ]; do
+            khal-personal list --format '{cancelled}{start-end-time-style} {title}' today 2d \
+                | ifne cat <(echo) /dev/stdin \
+                | ifne sponge "$events"
+            snooze -H'*' -M'*'
+        done
+    } &
+
+    generate_output
+    while rwc -e "$calendar" "$dates" "$events" "$width" >/dev/null 2>/dev/null; do
+        generate_output
     done
   '';
 
@@ -64,6 +97,14 @@ let
     : "''${XDG_RUNTIME_DIR:=/run/user/$(id -u)}"
     output="$XDG_RUNTIME_DIR/dates-calendar/output.txt"
     cat "$output"
+  '';
+
+  stw-dates-calendar = pkgs.writeShellScript "stw-dates-calendar" ''
+    PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.rwc ]}:"$PATH"
+    : "''${XDG_RUNTIME_DIR:=/run/user/$(id -u)}"
+    output="$XDG_RUNTIME_DIR/dates-calendar/output.txt"
+    cat "$output" 2>/dev/null
+    rwc -e "$output" >/dev/null 2>&1
   '';
 in
 {
@@ -75,15 +116,15 @@ in
   services.stw.widgets.dates = {
     enable = false;
 
-    command = "dates-calendar";
+    command = stw-dates-calendar;
 
     text.font = "monospace:size=10";
-    window.color = config.xresources.properties."*color8";
-    text.color = config.xresources.properties."*foreground";
+    window.color = config.theme.colors.brightBlack;
+    text.color = config.theme.colors.foreground;
     window.position.x = "-0";
     window.position.y = 48;
     window.padding = 12;
-    update = 1;
+    update = -1;
 
     window.top = true;
   };
