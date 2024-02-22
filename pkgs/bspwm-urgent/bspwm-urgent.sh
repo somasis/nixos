7afadf95a6f76b761022013ea61213a963041a75
Kylie McClain <kylie@somas.is>
2024-02-22 11:57:29 -0500

    bspwm-urgent: improve style

--- pkgs/bspwm-urgent/bspwm-urgent.sh
+++ pkgs/bspwm-urgent/bspwm-urgent.sh
@@ -1,75 +1,98 @@
-# shellcheck shell=sh
+# shellcheck shell=bash
 
-set_window_variables() {
-    : "${id:?set_window_variables(): no \$id provided}"
+set -euo pipefail
 
-    class=$(xdotool getwindowclassname "${id}")
+# exports $node_class and $node_hash
+set_node_variables() {
+    : "${node:?set_node_variables(): no node provided}"
 
-    hash=$(printf '%s%s' "${id}" "${class}" | sha256sum)
-    hash=${hash%% *}
+    node_class=$(xdotool getwindowclassname "${node}")
+    node_hash=$(printf '%s%s' "${node}" "${node_class}" | sha256sum)
+    node_hash=${node_hash%% *}
+
+    export node_class node_hash
 }
 
+# exports $notification_id
 set_notification_variables() {
-    : "${hash:?set_notification_variables(): no \$hash provided}"
+    : "${node_hash:?set_notification_variables(): no node hash provided}"
+
+    local notification_id notification_file
 
     notification_id=
-    notification_file="${runtime}/${hash}.notification"
-    if [ -e "${notification_file}" ]; then
+    notification_file="${runtime}/${node_hash}.notification"
+    if [[ -e "${notification_file}" ]]; then
         notification_id=$(cat "${notification_file}")
     fi
+    export notification_id notification_file
 }
 
 notify() {
-    if [ -n "${NOTIFY_SOCKET}" ]; then
+    if [[ -n "${NOTIFY_SOCKET}" ]]; then
         systemd-notify "$@"
     fi
 }
 
 process() {
-    while IFS=' ' read -r _ _ _ id flag state; do
-        case "${flag}=${state}" in
-            urgent=on)
-                set_window_variables
-                set_notification_variables
+    local event="${1:?process(): no event given}"
+    shift
 
-                {
-                    got_id=false
+    case "${event}" in
+        node_flag)
+            # local node_monitor node_desktop node node_flag node_flag_state
+            # local node_monitor="$1"
+            local node_desktop="$2"
+            local node="$3"
+            local node_flag="$4"
+            local node_flag_state="$5"
 
-                    notify-send \
-                        --app-name="${class}" \
-                        --icon="${class}" \
-                        --action="Focus" \
-                        --wait \
-                        --print-id \
-                        ${notification_id:+--replace-id="${notification_id}"} \
-                        "${class}" \
-                        "is marked urgent" \
-                        | while IFS= read -r line; do
-                            if [ "${got_id}" = 'false' ]; then
-                                got_id=true
-                                printf '%s' "${line}" >"${notification_file}"
-                                continue
-                            fi
+            case "${node_flag}=${node_flag_state}" in
+                urgent=on)
+                    set_node_variables "${node}" # exports node_hash
+                    set_notification_variables "${node_hash}"
 
-                            bspc node -f "${id}"
-                            rm -f "${notification_file}"
-                            break
-                        done
-                } &
-                ;;
-            urgent=off)
-                set_window_variables
-                set_notification_variables
+                    # if current/focused desktop has urgent node then don't show notification
+                    if [[ "${node_desktop}" != "$(bspc query -D -n focused || :)" ]]; then
+                        {
+                            got_id=false
 
-                if [ -e "${notification_file}" ]; then
-                    # dunstify -C "${notification_id}"
-                    notify-send --replace-id="${notification_id}" -t 1 close
+                            notify-send \
+                                --app-name="${node_class}" \
+                                --icon="${node_class}" \
+                                --action="Focus" \
+                                --wait \
+                                --print-id \
+                                ${notification_id:+--replace-id="${notification_id}"} \
+                                "${node_class}" \
+                                "is marked urgent" \
+                                | while IFS= read -r line; do
+                                    if [[ "${got_id}" = 'false' ]]; then
+                                        got_id=true
+                                        printf '%s' "${line}" >"${notification_file}"
+                                        continue
+                                    fi
 
-                    rm -f "${notification_file}"
-                fi
-                ;;
-        esac
-    done
+                                    bspc node -f "${node}"
+                                    rm -f "${notification_file}"
+                                    break
+                                done
+                        } &
+                    fi
+                    ;;
+                urgent=off)
+                    set_node_variables "${node}"
+                    set_notification_variables "${node_hash}"
+
+                    if [[ -e "${notification_file}" ]]; then
+                        # dunstify -C "${notification_id}"
+                        notify-send --replace-id="${notification_id}" -t 1 close
+
+                        rm -f "${notification_file}"
+                    fi
+                    ;;
+            esac
+            ;;
+    esac
 }
 
 : "${NOTIFY_SOCKET:=}"
@@ -86,5 +109,8 @@ notify --status="Subscribing to bspwm node_flag events..."
 bspc subscribe node_flag \
     | {
         notify --ready
-        process
+        while IFS=" " read -r line; do
+            set -- "${line}"
+            process "$@"
+        done
     }
