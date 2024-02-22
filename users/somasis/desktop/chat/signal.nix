7380d3e4aabd69308f93d6dfe14f95b4524012a7
Kylie McClain <kylie@somas.is>
2024-02-22 11:51:59 -0500

    signal: improve secret handling

--- users/somasis/desktop/chat/signal.nix
+++ users/somasis/desktop/chat/signal.nix
@@ -18,46 +18,12 @@ let
     getExeName
     ;
 
-  signalPackage = pkgs.signal-desktop-beta;
+  signal = pkgs.signal-desktop-beta;
   signalWindowClassName = "Signal Beta";
-  signalDescription = signalPackage.meta.description;
+  signalDescription = signal.meta.description;
 
-  signal = pkgs.symlinkJoin {
-    name = "signal-desktop-with-pass";
-    paths = [
-      (pkgs.writeShellScriptBin (getExeName signalPackage) ''
-        set -eu
-        set -o pipefail
-
-        entry="${osConfig.networking.fqdnOrHostName}/signal-desktop"
-
-        mkdir -m 700 -p "''${XDG_CONFIG_HOME:=$HOME/.config}/${signalWindowClassName}"
-        rm -f "$XDG_CONFIG_HOME/${signalWindowClassName}"/config.json
-        mkfifo "$XDG_CONFIG_HOME/${signalWindowClassName}"/config.json
-
-        ${config.programs.password-store.package}/bin/pass "$entry" \
-            | ${config.programs.jq.package}/bin/jq -R '{
-                key: .,
-                mediaPermissions: true,
-                mediaCameraPermissions: true
-            }' \
-            > "$XDG_CONFIG_HOME/${signalWindowClassName}"/config.json &
-
-        ${pkgs.rwc}/bin/rwc -p "$XDG_CONFIG_HOME/${signalWindowClassName}"/config.json \
-            | ${pkgs.xe}/bin/xe -s 'rm -f "$XDG_CONFIG_HOME/${signalWindowClassName}"/config.json' &
-
-        e=0
-        (exec -a ${getExeName signalPackage} ${getExe signalPackage} "$@") || e=$?
-        kill $(jobs -p)
-        exit "$e"
-      '')
-
-      signalPackage
-    ];
-  };
-
-  signalName = getExeName signalPackage;
-  signalPath = "${signal}/bin/${signalName}";
+  signalExeName = getExeName signal;
+  signalExe = getExe signal;
 in
 {
   home.packages = [ signal ];
@@ -78,46 +44,136 @@ in
       spellCheck = true;
     });
 
-  services.dunst.settings.zz-signal = {
-    desktop_entry = "signal-desktop*";
+  services.dunst.settings = {
+    signal = {
+      desktop_entry = lib.replaceStrings [ " " ] [ "" ] (lib.toLower signalWindowClassName);
 
-    # Signal blue
-    background = "#3a76f0";
-    foreground = "#ffffff";
+      # Signal blue
+      background = "#3a76f0";
+      foreground = "#ffffff";
+    };
+
+    zz-signal-cassie = {
+      desktop_entry = lib.replaceStrings [ " " ] [ "" ] (lib.toLower signalWindowClassName);
+
+      summary = "Cassandra*";
+      background = "#7596ff";
+      foreground = "#ffffff";
+    };
   };
 
-  systemd.user.services.signal = {
-    Unit = {
-      Description = signalDescription;
-      PartOf = [ "graphical-session-autostart.target" ];
-      Wants = [ "tray.target" ];
 
-      StartLimitIntervalSec = 1;
-      StartLimitBurst = 1;
-      StartLimitAction = "none";
+  systemd.user = let secret = "${config.xdg.configHome}/${signalWindowClassName}/config.json"; in {
+    services.signal = {
+      Unit = {
+        Description = signal.meta.description;
+        PartOf = [ "graphical-session-autostart.target" ];
+        Wants = [ "tray.target" ];
+
+        Requires = [ "secret-signal.service" ];
+        PropagatesStopTo = [ "secret-signal.service" ];
+
+        StartLimitIntervalSec = 1;
+        StartLimitBurst = 1;
+        StartLimitAction = "none";
+
+      };
+      Install.WantedBy = [ "graphical-session-autostart.target" ];
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${signalExe} " + lib.cli.toGNUCommandLineShell { } {
+          start-in-tray = true;
+        };
+
+        Restart = "on-abnormal";
+
+        SyslogIdentifier = "signal";
+      };
     };
-    Install.WantedBy = [ "graphical-session-autostart.target" ];
 
-    Service = {
-      Type = "simple";
-      ExecStart = "${signalPath} " + lib.cli.toGNUCommandLineShell { } {
-        start-in-tray = true;
+    services.secret-signal = {
+      Unit = {
+        Description = "Provide a secret for Signal";
+        Upholds = [ "signal.service" ];
+        StopWhenUnneeded = true;
       };
 
-      Restart = "on-abnormal";
+      Service =
+        let
+          secret-signal = pkgs.writeShellScript "secret-signal" ''
+            : "''${1?no secret entry given}"
+            : "''${2?no secret file given}"
+
+            PATH=${lib.makeBinPath [ pkgs.coreutils config.programs.password-store.package ]}
+
+            # Check if the entry exists
+            if pass show "$1" >/dev/null 2>&1; then
+                # If it does, write it to the file.
+                pass show "$1" > "$2"
+            else
+                # The entry doesn't exist, so just make the secret file;
+                # we'll synchronize it back whenever we stop the service.
+                touch "$2"
+            fi
+          '';
+
+          secret-signal-sync = pkgs.writeShellScript "secret-signal-sync" ''
+            : "''${1?no secret entry given}"
+            : "''${2?no secret file given}"
+
+            PATH=${lib.makeBinPath [ pkgs.coreutils config.programs.password-store.package ]}
+
+            # Regardless of if the entry exists or not, sync the changes.
+            # If it exists and they have changed, sync the changes to the entry,
+            # otherwise create the entry.
+            if ! cmp -s <(pass show "$1" 2>/dev/null || :) "$2"; then
+                pass insert -m -f "$1" < "$2"
+            fi
+          '';
+        in
+        {
+          Type = "oneshot";
+          SyslogIdentifier = "secret-signal";
+
+          # Create the secret file from the entry.
+          ExecStart = ''${secret-signal} ''${ENTRY} %t/signal.secret'';
+
+          # Only create the secret link after a successful start.
+          ExecStartPost = ''ln -sf %t/signal.secret ''${CONFIG}'';
+
+          # Synchronize the file with the entry on changes.
+          ExecStop = ''${secret-signal-sync} ''${ENTRY} %t/signal.secret'';
+          RemainAfterExit = true;
+
+          # Remove the secret file.
+          ExecStopPost = ''rm -f ''${CONFIG} %t/signal.secret'';
+
+          ExecSearchPath = lib.makeBinPath [
+            pkgs.coreutils
+            config.programs.password-store.package
+          ];
+
+          Environment = [
+            ''"CONFIG=%E/${signalWindowClassName}/config.json"''
+            "ENTRY=${osConfig.networking.fqdnOrHostName}/signal-desktop"
+          ];
+        };
     };
   };
 
   services.sxhkd.keybindings."super + s" = pkgs.writeShellScript "signal" ''
     ${pkgs.jumpapp}/bin/jumpapp \
         -c ${lib.escapeShellArg signalWindowClassName} \
-        -i ${lib.escapeShellArg signalName} \
+        -i ${lib.escapeShellArg signalExeName} \
         -f ${pkgs.writeShellScript "start-or-switch" ''
             if ! ${pkgs.systemd}/bin/systemctl --user is-active -q signal.service; then
-                ${pkgs.systemd}/bin/systemctl --user start signal.service && sleep 2
+                ${pkgs.systemd}/bin/systemctl --user start signal.service
+                ${pkgs.systemd-wait}/bin/systemd-wait --user signal.service active
             fi
             ${config.systemd.user.services.signal.Service.ExecStart}
-        ''}  '';
+        ''}
+  '';
 
   # services.xsuspender.rules.signal = {
   #   matchWmClassGroupContains = "signal-desktop";
