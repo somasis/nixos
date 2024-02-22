2d17ed45d54e76e126e87ade312237034af01665
Kylie McClain <kylie@somas.is>
2024-02-22 11:11:03 -0500

    nix: tweak config, use extra- where appropriate

--- hosts/ilo.somas.is/nix.nix
+++ hosts/ilo.somas.is/nix.nix
@@ -54,7 +54,7 @@
       connect-timeout = 5;
       http-connections = 64;
       max-substitution-jobs = 64;
-      substituters = [
+      extra-substituters = [
         # Prefer HTTP nix-serve over the SSH tunnel to the server.
         # Faster for multiple missing-path queries.
         "http://localhost:5000"
@@ -69,7 +69,7 @@
         "https://cache.ngi0.nixos.org"
       ];
 
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "spinoza.7596ff.com-1:3evmjxB2owiKU1RcWMaVW7al/xdOG3QVqEEYwILPK1w="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
