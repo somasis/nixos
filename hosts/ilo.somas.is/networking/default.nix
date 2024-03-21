264d0acd3e8d13a9981612a44bd39df097e30bc1
Kylie McClain <kylie@somas.is>
2024-03-20 20:55:37 -0400

    networking: remove the unnecessary regulatory stuff

--- hosts/ilo.somas.is/networking/default.nix
+++ hosts/ilo.somas.is/networking/default.nix
@@ -39,47 +39,47 @@
   persist.directories = [ "/etc/NetworkManager/system-connections" ];
   cache.directories = [ "/var/lib/NetworkManager" ];
 
-  services.udev.extraRules =
-    let
-      # NOTE: We use `iw` here instead because `crda` doesn't seem to work properly?
-      update-reg = pkgs.writeShellScript "update-reg" ''
-        PATH=${pkgs.geoclue2-with-demo-agent}/libexec/geoclue-2.0/demos:${lib.makeBinPath [ pkgs.curl pkgs.iw pkgs.jc pkgs.jq ]}:"$PATH"
+  # services.udev.extraRules =
+  #   let
+  #     # NOTE: We use `iw` here instead because `crda` doesn't seem to work properly?
+  #     update-reg = pkgs.writeShellScript "update-reg" ''
+  #       PATH=${pkgs.geoclue2-with-demo-agent}/libexec/geoclue-2.0/demos:${lib.makeBinPath [ pkgs.curl pkgs.iw pkgs.jc pkgs.jq ]}:"$PATH"
 
-        location=$(
-            where-am-i \
-                -a 1 \
-                | jc --kv \
-                | jq -r '
-                    "latitude=\(.Latitude | rtrimstr("°") | @sh)",
-                    "longitude=\(.Longitude | rtrimstr("°") | @sh)"
-                '
-        )
-        eval "$location"
+  #       location=$(
+  #           where-am-i \
+  #               -a 1 \
+  #               | jc --kv \
+  #               | jq -r '
+  #                   "latitude=\(.Latitude | rtrimstr("°") | @sh)",
+  #                   "longitude=\(.Longitude | rtrimstr("°") | @sh)"
+  #               '
+  #       )
+  #       eval "$location"
 
-        COUNTRY=$(
-            curl -Lf \
-                --show-error \
-                --no-progress-meter \
-                --connect-timeout 60 \
-                --retry 10 \
-                --retry-delay 5 \
-                --compressed \
-                --get \
-                -d format='json' \
-                -d email='kylie+nixos@somas.is' \
-                -d zoom='3' \
-                -d lat="$latitude" \
-                -d lon="$longitude" \
-                'https://nominatim.openstreetmap.org/reverse' \
-                | jq -r '.address.country_code | ascii_upcase'
-        )
+  #       COUNTRY=$(
+  #           curl -Lf \
+  #               --show-error \
+  #               --no-progress-meter \
+  #               --connect-timeout 60 \
+  #               --retry 10 \
+  #               --retry-delay 5 \
+  #               --compressed \
+  #               --get \
+  #               -d format='json' \
+  #               -d email='kylie+nixos@somas.is' \
+  #               -d zoom='3' \
+  #               -d lat="$latitude" \
+  #               -d lon="$longitude" \
+  #               'https://nominatim.openstreetmap.org/reverse' \
+  #               | jq -r '.address.country_code | ascii_upcase'
+  #       )
 
-        exec iw reg set "$COUNTRY"
-      '';
-    in
-    ''
-      KERNEL=="regulatory*", ACTION=="change", SUBSYSTEM=="platform", RUN+="${update-reg}"
-    '';
+  #       exec iw reg set "$COUNTRY"
+  #     '';
+  #   in
+  #   ''
+  #     KERNEL=="regulatory*", ACTION=="change", SUBSYSTEM=="platform", RUN+="${update-reg}"
+  #   '';
 
   # TODO: Track net usage by services
   #       Currently cannot by used for user services...
